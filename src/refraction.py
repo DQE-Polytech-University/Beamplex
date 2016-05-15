2d2ff24573b2c2e71c6bcf988a7e17d0bec37c11
Maxim <max_gc@mail.ru>
2016-05-16 00:33:07 +0300

    added computeRefraction() in refraction

--- src/refraction.py
+++ src/refraction.py
@@ -2,22 +2,23 @@ h = 4.1356 * 10 ** (-15)
 cl = 2.998 * 10 ** 14
 
 class RefractionCalc:
-    conc = []
-    refract = []
-    lyambda0 = 0
+    concentration = []
+    refraction = []
+    wavelength = 0
     
-    def __init__(self, lyambda, concentration, refraction):
-        self.conc = concentration
-        self.refract = refraction
-        self.lyambda0 = lyambda
+    def __init__(self, lyambda, layersNumber, concentration):
+        self.concentration = concentration
+        self.wavelength = lyambda
+        self.layersNumber = layersNumber
+        
         
     def refraction_AlGaAs(self, concentration):	#index of refraction Al_x Ga_1-x As calculation:
         A0 = 6.3 + 19 * concentration
         B0 = 9.4 - 10.2 * concentration
         E0 = 1.425 + 1.155 * concentration + 0.37 * concentration ** 2
         delta0 = 0.34 - 0.04 * concentration
-        chi = h * cl / (self.lyambda0 * E0)
-        chiSO = h * cl / (self.lyambda0 * (E0 + delta0))
+        chi = h * cl / (self.wavelength * E0)
+        chiSO = h * cl / (self.wavelength * (E0 + delta0))
         fSO =  (2 - (1 + chiSO) ** 0.5 - (1 - chiSO) ** 0.5)/ chiSO ** 2
         f = (2 - (1 + chi) ** 0.5 -(1 - chi) ** 0.5)/ chi ** 2
         n = (A0 * (f + fSO/2 * (E0 / (E0 + delta0)) ** 1.5) + B0) ** 0.5
@@ -29,5 +30,14 @@ class RefractionCalc:
         C = 0.6245
         Eggaas = 1.424
         E00 = 1.424 - 1.56 * concentration + 0.494 * concentration ** 2
-        n = (A + B/ (1 - (C * Eggaas / (self.lyambda0 * E00)) ** 2)) ** 0.5
+        n = (A + B/ (1 - (C * Eggaas / (self.wavelength * E00)) ** 2)) ** 0.5
         return n
+
+    def computeRefraction(self):
+        for i in range(self.layersNumber):
+            if i == (self.layersNumber / 2):
+                self.refraction.append(self.refraction_InGaAs(self.concentration[i]))
+            else:
+                self.refraction.append(self.refraction_AlGaAs(self.concentration[i]))
+
+
