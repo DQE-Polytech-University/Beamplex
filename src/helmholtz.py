0fe5f2981f165f167fc70ef927fc567957dca243
Maxim <max_gc@mail.ru>
2016-05-24 22:44:53 +0300

    some corrections

--- src/helmholtz.py
+++ src/helmholtz.py
@@ -24,8 +24,8 @@ class HelmholtzSolver:
         self.lyambda0 = lyambda
         self.thickness = thickness
         self.refraction = refr
-        self.deltaX = (sum((int(self.thickness[i]) for i in range(0, int(len(self.thickness))))) - 0.001) / float(self.matrix_dimension)
-        self.deltaArb = self.deltaX * 2 * math.pi / self.lyambda0
+        self.deltaX = (sum((self.thickness[i] for i in range(0, int(len(self.thickness))))) - 0.001) / float(self.matrix_dimension) 
+        self.deltaArb = float(self.deltaX * 2 * math.pi) / float(self.lyambda0)
         self.gridX = [i * self.deltaX for i in range(0, self.matrix_dimension + 1)]
         for i in self.gridX:
             if 0 <= i and i < self.thickness[0]:
@@ -40,12 +40,12 @@ class HelmholtzSolver:
                 self.gridN.append(self.refraction[4])
         
     def refractionMatrix(self):
-        self.Mtr = np.zeros((self.matrix_dimension + 1, self.matrix_dimension + 1))
+        self.Mtr = np.zeros((self.matrix_dimension + 1, self.matrix_dimension + 1))             
         flat = self.Mtr.ravel()
         flat[self.matrix_dimension + 1 :: self.matrix_dimension + 2] = (1/self.deltaArb) ** 2
         flat[1 :: self.matrix_dimension + 2] = (1/self.deltaArb) ** 2
         for i in range(self.matrix_dimension + 1):
-            flat[0 :: self.matrix_dimension + 2] = (self.gridN[i]**2) - 2 / (self.deltaArb) ** 2
+            flat[0 :: self.matrix_dimension + 2] = (self.gridN[i] ** 2) - 2 / (self.deltaArb) ** 2 
         
     def find_neffective(self):
         neffective = [x for x in range(self.matrix_dimension + 1)]
@@ -53,7 +53,7 @@ class HelmholtzSolver:
         matric = linalg.eig(self.Mtr)                            #gives the eigenvalues and right eigenvectors of a square array
         neffectiv = matric[0]                                    #returns array of eigenvalues
         for k in range(self.matrix_dimension + 1):                   #gives squared root
-            self.neffect[k] = sqrt(neffectiv[k]).real            #and returns real part
+            self.neffect[k] = (sqrt(neffectiv[k])).real            #and returns real part
     
     def find_max(self):
         neff_max = max(self.neffect)                             #gives the maximum element of index refraction matrix
