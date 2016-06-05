7706052c287445ab3dcee2d9070c9d8a9a3e9795
IvanSavelyev <ivsound93@gmail.com>
2016-06-05 19:41:53 +0300

    Add files via upload

--- src/laserstructure.py
+++ src/laserstructure.py
@@ -6,11 +6,37 @@ class Laser:
     laserField = []
 
     def __init__(self, wavelength, layersNumber, concentration, thickness):
+
+        if isinstance(wavelength, (int, float)) == False:
+            raise TypeError("wavelength should be a number")
+        if isinstance(layersNumber, (int, float)) == False:
+            raise TypeError("layersNumber should be a number")
+        if isinstance(concentration, list) == False:
+            raise TypeError("concentration should be a list")
+        if isinstance( thickness, (list)) == False:
+            raise TypeError("thickness should be a list")
+        for i in range(layersNumber):
+            if isinstance(concentration[i], (int, float)) == False or isinstance( thickness[i], (int, float)) == False:
+                raise TypeError("concentration and thickness elements should be numbers")
+
+        if wavelength is None:
+            raise ValueError("wavelength is undefined")
+        if layersNumber is None:
+            raise ValueError("layersNumber is undefined")
+        if concentration is None:
+            raise ValueError("concentration is undefined")
+        if thickness is None:
+            raise ValueError("thickness is undefined")
+            
+        if wavelength < 0.85 or wavelength > 1.5:
+            raise ValueError("wavelength out of range")
+        
         self.laserWavelength = wavelength
         self.laserLayersNumber = layersNumber
         self.laserConcentration = concentration
         self.laserThickness = thickness
-    
+
+
     def plotRefraction(self):
         coord = [0]
         sum = 0
@@ -26,5 +52,3 @@ class Laser:
                 refr.append(self.laserRefraction[i])
         plt.plot(coord, refr, 'b')
         plt.show()
-
-
