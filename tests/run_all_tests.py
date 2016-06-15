1c43f879b5f103d10bab6a91861c2c935ff65407
ilayko <ilay-kim@ya.ru>
2016-06-15 18:57:26 +0300

    Create run_all_tests.py

---
+++ tests/run_all_tests.py
@@ -0,0 +1,10 @@
+import os
+
+os.system('python jsonloader_test.py')
+os.system('python laserstructure_test_input.py')
+os.system('python laserstructure_test_output.py')
+os.system('python refraction_test_input.py')
+os.system('python refraction_test_output.py')
+os.system('python helmholtz_test_input.py')
+os.system('python helmholtz_test_output.py')
+os.system('python main_test.py')
